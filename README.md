d6ccf3c0987f4abaa57dd52a9a28eeb49fdebaaa
darryncampbell <darryncampbell@hotmail.com>
2020-06-10 16:47:57 +0100

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # EMDK-DeviceIdentifiers-Sample
-How to access device identifiers such as serial number, device ID and IMEI
+
+How to access device identifiers such as serial number and IMEI on Zebra devices running Android 10
+
+Android 10 limited access to device identifiers for all apps running on the platform regardless of their target API level.  As explained in the docs for [Android 10 privacy changes](https://developer.android.com/about/versions/10/privacy/changes) this includes the serial number, IMEI and some other identifiable information.
+
+**Zebra mobile computers running Android 10 are able to access both the serial number and IMEI** however applications need to be **explicitly granted the ability** to do so and use a proprietary API.
+
+To access the serial number and IMEI file on Zebra Android devices running Android 10 or higher, first declare a new permission in your AndroidManifest.xml
+
+```xml
+<uses-permission android:name="com.zebra.provider.READ"/>
+```
+
+Then use the [MX access manager](https://techdocs.zebra.com/mx/accessmgr/) to allow your application to call the service identifiers associated with the serial number and IMEI
+
+The MX access manager settings to enable this are as follows:
+- Service Access Action: "AllowCaller" (or 'Allow Caller to Call Service')
+- Service Identifier: For the serial number use content://oem_info/oem.zebra.secure/build_serial.  For the IMEI use content://oem_info/wan/imei.  If you want to allow your app access to both, you will need to declare two different instances of the AccessManager.
+- Caller Package Name: Your package name, in the case of this sample it is com.zebra.emdk_deviceidentifiers_sample.
+- Caller Signature: The signing certificate of your application.  For more information on generating this see https://github.com/darryncampbell/MX-SignatureAuthentication-Demo.
+
+You can apply the MX access manager settings in one of three ways:
+1. Via StageNow
+2. Via your EMM
+3. Via your application, using the EMDK Profile Manager.
+
+For example, StageNow will look as follows to enable access to the serial number:
+
+![StageNow](https://raw.githubusercontent.com/darryncampbell/EMDK-DeviceIdentifiers-Sample/master/screenshots/stagenow.png)
+
+You can then run this sample app and should see something like the below (captured on a device that does not support a SIM card):
+
+![Non-WAN](https://raw.githubusercontent.com/darryncampbell/EMDK-DeviceIdentifiers-Sample/master/screenshots/non-wan.jpg)
+
+## Handling errors:
+
+If you failed to correctly allow your application access to oem_info service, you will see an error stating so against each property you did not assign access to, as shown below:
+
+![no_service_access](https://raw.githubusercontent.com/darryncampbell/EMDK-DeviceIdentifiers-Sample/master/screenshots/no_service_access.jpg)
+
+Assign access to your device and re-run the application.
+
+
---
+++ app/src/main/java/com/zebra/emdk_deviceidentifiers_sample/MainActivity.java
@@ -0,0 +1,77 @@
+package com.zebra.emdk_deviceidentifiers_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.content.pm.SigningInfo;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Trace;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "DeviceID";
+    String URI_SERIAL = "content://oem_info/oem.zebra.secure/build_serial";
+    String URI_IMEI = "content://oem_info/wan/imei";
+
+    //  todo are other APIs documented?
+    //  todo test on WAN device
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RetrieveOEMInfo(Uri.parse(URI_SERIAL), (TextView) findViewById(R.id.txtSerialNumber), false);       //  Build.getSerial()
+        RetrieveOEMInfo(Uri.parse(URI_IMEI), (TextView) findViewById(R.id.txtImei), true);              //  TelephonyManager getImei()
+}
+
+    private void RetrieveOEMInfo(Uri uri, TextView status, boolean isIMEI) {
+        //  For clarity, this code calls ContentResolver.query() on the UI thread but production code should perform queries asynchronously.
+        //  See https://developer.android.com/guide/topics/providers/content-provider-basics.html for more information
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        if (cursor == null || cursor.getCount() < 1)
+        {
+            String errorMsg = "Error: This app does not have access to call OEM service. " +
+              "Please assign access to " + uri + " through MX.  See ReadMe for more information";
+            Log.d(TAG, errorMsg);
+            status.setText(errorMsg);
+            return;
+        }
+        while (cursor.moveToNext()) {
+            if (cursor.getColumnCount() == 0)
+            {
+                //  No data in the cursor.  I have seen this happen on non-WAN devices
+                String errorMsg = "Error: " + uri + " does not exist on this device";
+                Log.d(TAG, errorMsg);
+                if (isIMEI)
+                    errorMsg = "Error: Could not find IMEI.  Is device WAN capable?";
+                status.setText(errorMsg);
+            }
+            else{
+                for (int i = 0; i < cursor.getColumnCount(); i++) {
+                    Log.v(TAG, "column " + i + "=" + cursor.getColumnName(i));
+                    try {
+                        String data = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(i)));
+                        Log.i(TAG, "Column Data " + i + "=" + data);
+                        status.setText(data);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.i(TAG, "Exception reading data for column " + cursor.getColumnName(i));
+                    }
+                }
+            }
+        }
+        cursor.close();
+    }
+}
